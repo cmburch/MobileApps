87aa2cac416b827941fea4f9a28ebe135c89e97d
Charles Burch <charles_burch89@yahoo.com>
2021-01-07 01:45:27 -0500

    add minor notes and override decorator

--- flutter_dart_course/test_app/lib/main.dart
+++ flutter_dart_course/test_app/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  //runApp calls the build method and renders the widgets on the screen.
+  runApp(MyApp()); //equivalent to new MyApp()
 }
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Text('Hello World'),
