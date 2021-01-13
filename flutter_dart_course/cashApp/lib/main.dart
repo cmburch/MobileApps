3417898871ea906d8dbc83fe99b6a01e26ff9f34
Charles Burch <charles_burch89@yahoo.com>
2021-01-13 02:30:14 -0500

    [cashapp] add intl dateFormat package and style the Date in card

--- flutter_dart_course/cashApp/lib/main.dart
+++ flutter_dart_course/cashApp/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+//custom widgets
 import './transaction.dart';
 
 void main() => runApp(MyApp());
@@ -84,7 +86,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          tx.date.toString(),
+                          DateFormat.yMMMd().format(tx.date),
                           style: TextStyle(
                             color: Colors.grey,
                           ),
@@ -102,62 +104,110 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+
+
+
+
 // import 'package:flutter/material.dart';
 // import './transaction.dart';
 
 // void main() => runApp(MyApp());
 
 // class MyApp extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: 'Flutter App',
-//      home: MyHomePage(),
-//    );
-//  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter App',
+//       home: MyHomePage(),
+//     );
+//   }
 // }
 
 // class MyHomePage extends StatelessWidget {
-//  final List<Transaction> transactions = [
-//    Transaction(
-//      id: 't1',
-//      title: 'New Shoes',
-//      amount: 69.99,
-//      date: DateTime.now(),
-//    ),
-//    Transaction(
-//      id: 't2',
-//      title: 'Weekly Groceries',
-//      amount: 16.53,
-//      date: DateTime.now(),
-//    ),
-//  ];
+//   final List<Transaction> transactions = [
+//     Transaction(
+//       id: 't1',
+//       title: 'New Shoes',
+//       amount: 69.99,
+//       date: DateTime.now(),
+//     ),
+//     Transaction(
+//       id: 't2',
+//       title: 'Weekly Groceries',
+//       amount: 16.53,
+//       date: DateTime.now(),
+//     ),
+//   ];
 
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text('Flutter App'),
-//      ),
-//      body: Column(
-//        mainAxisAlignment: MainAxisAlignment.spaceAround,
-//        crossAxisAlignment: CrossAxisAlignment.stretch,
-//        children: <Widget>[
-//          Container(
-//            width: double.infinity,
-//            child: Card(
-//              color: Colors.blue,
-//              child: Text('CHART!'),
-//              elevation: 5,
-//            ),
-//          ),
-//          Column(
-//            children: transactions.map((tx) {
-//              return Card(child: Text(tx.title));
-//            }).toList(),
-//          ),
-//        ],
-//      ),
-//    );
-//  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Flutter App'),
+//       ),
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.spaceAround,
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         children: <Widget>[
+//           Container(
+//             width: double.infinity,
+//             child: Card(
+//               color: Colors.blue,
+//               child: Text('CHART!'),
+//               elevation: 5,
+//             ),
+//           ),
+//           Column(
+//             children: transactions.map((tx) {
+//               return Card(
+//                 child: Row(
+//                   children: [
+//                     Container(
+//                       margin: EdgeInsets.symmetric(
+//                         vertical: 10,
+//                         horizontal: 15,
+//                       ),
+//                       decoration: BoxDecoration(
+//                         border: Border.all(
+//                           color: Colors.purple,
+//                           width: 2,
+//                         ),
+//                       ),
+//                       padding: EdgeInsets.all(10),
+//                       child: Text(
+//                         '\$${tx.amount}',
+//                         style: TextStyle(
+//                           fontWeight: FontWeight.bold,
+//                           fontSize: 20,
+//                           color: Colors.purple,
+//                         ),
+//                       ),
+//                     ),
+//                     Column(
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         Text(
+//                           '\$${tx.amount}',
+//                           style: TextStyle(
+//                             fontSize: 16,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                         Text(
+//                           tx.date.toString(),
+//                           style: TextStyle(
+//                             color: Colors.grey,
+//                           ),
+//                         ),
+//                       ],
+//                     )
+//                   ],
+//                 ),
+//               );
+//             }).toList(),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
 // }
