323306ef0b5b90b8b941e58151214a6d0a799aca
Charles Burch <charles_burch89@yahoo.com>
2021-10-07 12:12:48 -0400

    create state for the MyApp main widget

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -116,28 +116,45 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  int questionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var questions = ["what is your favorite color", "what is your favorite animal"];
+    var questions = [
+      "what is your favorite color",
+      "what is your favorite animal"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first App'),
         ),
         body: Column(children: [
-          Text("The Questions"),
+          Text(questions[questionIndex]),
           RaisedButton(
             child: Text("Answer 1"),
-            onPressed: null,
+            onPressed: answerQuestion,
           ),
           RaisedButton(
             child: Text("Answer 2"),
-            onPressed: null,
+            onPressed: () => print("Answer question 2"),
           ),
           RaisedButton(
             child: Text("Answer 3"),
-            onPressed: null,
+            onPressed: () => print("Answer question 3"),
           ),
         ]),
       ),
