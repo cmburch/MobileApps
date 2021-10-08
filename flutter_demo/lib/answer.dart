1ec92c9ef298ed4cd3fce094191bab459e12e48d
Charles Burch <charles_burch89@yahoo.com>
2021-10-08 03:42:25 -0400

    created custom widget to display answers

---
+++ flutter_demo/lib/answer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final String answerText;
+  final VoidCallback selectHandler;
+
+  Answer({this.selectHandler, this.answerText});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text(
+          answerText,
+        ),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
