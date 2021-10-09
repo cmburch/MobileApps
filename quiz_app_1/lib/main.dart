18c472e0abfb42ef9d13b41d2aa0109f47676195
Charles Burch <charles_burch89@yahoo.com>
2021-10-09 08:36:13 -0400

    add state variables and center text in appbar

--- quiz_app_1/lib/main.dart
+++ quiz_app_1/lib/main.dart
@@ -12,14 +12,50 @@ class QuizApp extends StatefulWidget {
 }
 
 class QuizAppState extends State<QuizApp> {
+  static const String _appTitle = "Quiz App";
+  final int _questionIndex = 0;
+  final int _score = 0;
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'text': 'Teacher', 'score': 1},
+        {'text': 'Teacher', 'score': 1},
+        {'text': 'Teacher', 'score': 1},
+        {'text': 'Teacher', 'score': 1},
+      ],
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
+    final bool _isQuizFinished = _questionIndex >= _questions.length;
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Container(
+            alignment: Alignment.center,
             width: double.infinity,
-            child: const Text("Quiz App"),
+            child: const Text(_appTitle),
           ),
         ),
       ),
