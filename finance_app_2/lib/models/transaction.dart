89a3b9eaef453e795ea263cfa568459a036dfef3
Charles Burch <charles_burch89@yahoo.com>
2021-11-03 17:40:33 -0400

    [finance_app_2] create transaction model class

---
+++ finance_app_2/lib/models/transaction.dart
@@ -0,0 +1,14 @@
+class Transaction {
+  // id,title,amount,date
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({
+     required this.id,
+     required this.title,
+     required this.amount,
+     required this.date
+  })
+}
