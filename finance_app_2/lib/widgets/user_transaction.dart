c0cb64ad11f4896619db654aa952190c80ca453d
Charles Burch <charles_burch89@yahoo.com>
2021-11-03 18:31:16 -0400

    [finance_app_2] created the inital state for user transactions

---
+++ finance_app_2/lib/widgets/user_transaction.dart
@@ -0,0 +1,39 @@
+import 'package:finance_app_2/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+class UserTransaction extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _UserTransactionState();
+  }
+}
+
+class _UserTransactionState extends State<UserTransaction> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addTransaction(String txTitle, double txAmount) {
+    final newTransaction = Transaction(
+        id: DateTime.now().toString(),
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
