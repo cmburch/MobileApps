34d966027b428f423c1539dda86f04fa76972728
Charles Burch <charles_burch89@yahoo.com>
2021-11-09 15:41:44 -0500

    [finance_app_2] add placeholder image for list with no created transactions

--- finance_app_2/lib/widgets/transaction_list.dart
+++ finance_app_2/lib/widgets/transaction_list.dart
@@ -11,7 +11,26 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
+      child: userTransactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            )
+          : ListView.builder(
         itemCount: userTransactions.length,
         itemBuilder: (BuildContext context, int index) {
           return Card(
